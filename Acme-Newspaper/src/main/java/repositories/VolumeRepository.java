1e55c845f3967b1d8e3830d2caa60c5dffd70435
kain90 <utri1990@gmail.com>
2018-04-26 10:43:19 +0200

    Repositories - VolumeSubscription
    Creado método findAvailableVolumesByCustomerId para mostrar aquellos volumes disponibles para un usuario.

--- Acme-Newspaper/src/main/java/repositories/VolumeRepository.java
+++ Acme-Newspaper/src/main/java/repositories/VolumeRepository.java
@@ -1,12 +1,18 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Volume;
 
 @Repository
 public interface VolumeRepository extends JpaRepository<Volume, Integer> {
+
+	@Query("select vl from Volume vl where vl.id not in (select vs.volume.id from Customer c join c.volumeSubscriptions vs where c.id = ?1)")
+	Collection<Volume> findAvailableVolumesByCustomerId(int customerId);
 
 }
