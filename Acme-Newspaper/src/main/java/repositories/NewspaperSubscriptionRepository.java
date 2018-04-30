e4af0dd4c9406a0dbedaf8463a87a667d2a1a363
kain90 <utri1990@gmail.com>
2018-04-30 10:27:07 +0200

    Repositories - NewspaperSubscription

--- Acme-Newspaper/src/main/java/repositories/NewspaperSubscriptionRepository.java
+++ Acme-Newspaper/src/main/java/repositories/NewspaperSubscriptionRepository.java
@@ -5,10 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import domain.Customer;
+import domain.Newspaper;
 import domain.NewspaperSubscription;
 
 @Repository
 public interface NewspaperSubscriptionRepository extends JpaRepository<NewspaperSubscription, Integer> {
+
+	@Query("select subs.newspaper from Customer c join c.newspaperSubscriptions subs where c.id = ?1 and subs.newspaper.id = ?2")
+	NewspaperSubscription findNewspaperSubscriptionByCustomerIdAndNewspaperId(int customerId, int newspaperId);
 
 	// Dashboard queries -------------------------------------------------------
 
@@ -19,7 +24,7 @@ public interface NewspaperSubscriptionRepository extends JpaRepository<Newspaper
 	@Query("select case when (count(subs.newspaper) > 0) then true else false end from Customer c join c.newspaperSubscriptions subs where c.id = ?1 and subs.newspaper.id = ?2")
 	boolean thisCustomerCanSeeThisNewspaper(int customerId, int newspaperId);
 
-	@Query("select case when (count(subs) > 0) then true else false end from NewspaperSubscription subs where subs.customer.id = ?1 and subs.newspaper.id = ?2")
-	boolean isThisCustomerSubscribeOnThisNewspaper(int customerId, int newspaperId);
+	@Query("select case when (count(subs) > 0) then true else false end from NewspaperSubscription subs where subs.customer = ?1 and subs.newspaper = ?2")
+	boolean isThisCustomerSubscribeOnThisNewspaper(Customer customer, Newspaper newspaper);
 
 }
