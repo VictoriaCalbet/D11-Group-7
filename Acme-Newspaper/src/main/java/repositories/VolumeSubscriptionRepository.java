66927e3c4564573e12f05e5f019e6513eeca374f
kain90 <utri1990@gmail.com>
2018-04-30 13:42:02 +0200

    Repositories - VolumeSubscriptionRepository

--- Acme-Newspaper/src/main/java/repositories/VolumeSubscriptionRepository.java
+++ Acme-Newspaper/src/main/java/repositories/VolumeSubscriptionRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import domain.Customer;
+import domain.Volume;
 import domain.VolumeSubscription;
 
 @Repository
@@ -12,4 +14,7 @@ public interface VolumeSubscriptionRepository extends JpaRepository<VolumeSubscr
 
 	@Query("select count(vs)/(select count(ns) from NewspaperSubscription ns) from VolumeSubscription vs")
 	Double ratioOfVolumeSubscriptionsVsNewspaperSubscription();
+
+	@Query("select case when (count(subs) = 1) then true else false end from VolumeSubscription subs where subs.customer = ?1 and subs.volume = ?2")
+	boolean isThisCustomerSubscribeOnThisVolume(Customer customer, Volume volume);
 }
